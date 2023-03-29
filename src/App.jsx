1baa711ef2a0b475eabeb1820e14e22691b45cf2
Paul Edwin Antonio Juan <l19350281@tuxtepec.tecnm.mx>
2023-03-28 18:05:44 -0600

    Grid build

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+function App() {
+  return (
+    <div class="container text-center">
+      
+      <div class="row">
+        <div class="col">
+          <h1>SHopping List</h1>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col">
+          Element 1
+        </div>
+        <div class="col">
+          Button
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col">
+          Element 2
+        </div>
+        <div class="col">
+          Button
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
